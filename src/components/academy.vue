1a7c830cc9c9d994edd81095c1a49d9898cc7ece
Victor Monteiro Cunha <victor.monteiro.cunha@gmail.com>
2020-04-15 21:17:11 -0300

    Academy

--- src/components/academy.vue
+++ src/components/academy.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    <h2 class="heading">Formação</h2>
+    <div class="card">
+      <div
+        class="place"
+        v-for="(place, index) in this.data"
+        v-bind:key="place.name"
+        :class="{'last' : index == data.length - 1, 'first' : index == 0}"
+      >
+        <div class="picture">
+          <img :src="place.image" :alt="place.name" />
+        </div>
+        <h1 class="name">{{place.name}}</h1>
+        <span class="description">{{place.description}}</span>
+        <i class="start">De: {{ place.start }}</i>
+        <i class="end">Até: {{ place.end }}</i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +35,44 @@ export default {
 };
 </script>
 
-<style>
+<style lang="sass" scoped>
+@import "@/assets/sass/app.sass"
+.card
+  .place
+    display: grid
+    grid-template-areas: 'picture name name start' 'picture description description end'
+    grid-template-columns: 120px repeat(3, auto)
+    gap: 1rem
+    border-bottom: 1px solid map-get($colors, primary)
+    padding: 20px 0
+
+    &.first
+      padding-top: 0
+
+    &.last
+      border: 0
+      padding-bottom: 0
+
+    .picture
+      grid-area: picture
+      img
+        width: 120px
+
+    .name
+      grid-area: name
+
+    .description
+      grid-area: description
+
+    .start
+      grid-area: start
+      justify-content: flex-end
+
+    .end
+      grid-area: end
+      justify-content: flex-end
+
+    > *
+      display: flex
+      align-items: center
 </style>
--- src/state/academy.js
+++ src/state/academy.js
@@ -2,14 +2,14 @@ export default [
   {
     name:
       "Colégio Técnico Industrial Professor Isaac Portal Roldán - Bauru, SP",
-    image: "",
+    image: "cti.png",
     description: "Ensino médio e Ensino Técnico de Informática",
     start: "2013",
     end: "2015"
   },
   {
-    name: "Universidade do Sagrado Coração - Bauru, SP",
-    image: "",
+    name: "UNISAGRADO - Bauru, SP",
+    image: "usc.png",
     description: "Ensino Superior – Bacharelado de Ciências da Computação",
     start: "2016",
     end: "2019"
