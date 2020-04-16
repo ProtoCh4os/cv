2ba0069e9383a7a2e9c18b3de85a0ded4018a42d
Victor Monteiro Cunha <victor.monteiro.cunha@gmail.com>
2020-04-15 21:51:01 -0300

    Abilities

--- src/components/abilities.vue
+++ src/components/abilities.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <h2 class="heading">Experiência Profissional</h2>
+    <div class="card">
+      <div class="ability" v-for="(ability, index) in this.data" v-bind:key="index" 
+        :class="{'last' : index == data.length - 1, 'first' : index == 0}"
+      >
+        <font-awesome-icon class="icon" size="2x" :icon="ability.image" />
+        <span v-html="ability.description" class="title"></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +27,32 @@ export default {
 };
 </script>
 
-<style>
+<style lang="sass" scoped>
+@import "@/assets/sass/app.sass"
+
+.card
+  display: flex
+  flex-direction: column
+
+  .ability
+    display: grid
+    grid-template-areas: 'icon title'
+    grid-template-columns: 50px auto
+    padding: 20px 0
+    gap: 2rem
+    align-items: center
+
+    .icon
+      grid-area: icon
+      width: 50px
+      height: auto
+
+    .title
+      grid-area: title
+
+    .first
+      padding-top: 0;
+
+    .last
+      padding-bottom: 0;
 </style>
