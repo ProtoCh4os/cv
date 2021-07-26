4bd52d566e7035b1687eaf673191326d90b44bbb
victor <victor.cunha@experiencesolucoes.com.br>
2021-07-26 15:50:42 -0300

    update experience

--- src/state/experience.js
+++ src/state/experience.js
@@ -1,44 +1,51 @@
 export default [
   {
+    image: "cbm.png",
+    company: "Construtora Barbosa Mello",
+    position: "Analista de TI",
+    start: "2021-08-09",
+    end: false,
+  },
+  {
     image: "deltatic.png",
-    company: "Experience Soluções",
-    position: "Desenvolvedor Junior II",
+    company: "Deltatic",
+    position: "Desenvolvedor Pleno II",
     start: "2021-02-23",
-    end: false
+    end: "2021-07-15",
   },
   {
     image: "experience.png",
-    company: "Deltatic",
-    position: "Desenvolvedor Pleno II",
+    company: "Experience Soluções",
+    position: "Desenvolvedor Junior II",
     start: "2020-04-15",
-    end: "2021-02-23"
+    end: "2021-02-23",
   },
   {
     image: "neo.png",
     company: "Neoassist S/A",
     position: "Desenvolvedor PHP Júnior",
     start: "2019-06-01",
-    end: "2020-04-01"
+    end: "2020-04-01",
   },
   {
     image: "neo.png",
     company: "Neoassist S/A",
     position: "Estágio – Desenvolvedor PHP",
     start: "2018-10-01",
-    end: "2019-06-01"
+    end: "2019-06-01",
   },
   {
     image: "arca.png",
     company: "Arca Solutions Inc",
     position: "Desenvolvedor Júnior de Suporte",
     start: "2018-05-01",
-    end: "2018-06-01"
+    end: "2018-06-01",
   },
   {
     image: "arca.png",
     company: "Arca Solutions Inc",
     position: "Estágio – Desenvolvedor de Suporte",
     start: "2015-05-01",
-    end: "2018-02-01"
-  }
+    end: "2018-02-01",
+  },
 ];
